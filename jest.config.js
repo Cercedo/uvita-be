c5a873dd4050c83345ac7bd9d87563b6353f8b92
Cercedo <marloncercedo@gmail.com>
2024-07-20 07:00:56 -0500

    chore: add Jest configuration to handle import alias

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 /* global module */
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
   testEnvironment: 'node',
   transform: {
     '^.+.tsx?$': ['ts-jest', {}],
