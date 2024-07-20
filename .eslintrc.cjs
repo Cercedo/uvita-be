b6bcd7dc72a730d63c7726d0f8cfb0894885c08f
Cercedo <marloncercedo@gmail.com>
2024-07-19 22:09:34 -0500

    chore: add extra settings to ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,29 @@
 /* eslint-env node */
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules'],
+  rules: {
+    'no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: true,
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'import/no-cycle': 'error',
+    eqeqeq: 'error',
+    'no-eval': 'error',
+    curly: 'error',
+    'prefer-const': 'error',
+    'no-var': 'error',
+  },
 };
