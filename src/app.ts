896b4110f182e85ae8907187b9c7f1a792257454
Cercedo <marloncercedo@gmail.com>
2024-07-20 20:37:11 -0500

    feat: serve static files

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import { startGreeting } from '@/utils';
 
 export const app: express.Express = express();
 
+app.use(express.static('public'));
+
 app.get('/', (request: Request, response: Response) => {
   startGreeting();
   response.send({ data: 'Hello world! 🍏' });
